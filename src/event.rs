afb421bb12c0108779dab247e1afeaf3a5240dcf
pikol93 <pikol93@wp.pl>
2023-10-29 23:27:21 +0100

    Added Event

---
+++ src/event.rs
@@ -0,0 +1,49 @@
+use std::cmp::Ordering;
+use chrono::{DateTime, Utc};
+use crate::person::Policeman;
+
+#[derive(Debug, Copy, Clone)]
+pub enum EventAction {
+    PolicemanEmployment,
+    PolicemanResignation,
+    Report,
+    SendPatrol(usize),
+    FinishedPatrol(usize),
+    Snapshot(&'static str, bool),
+}
+
+
+#[derive(Debug, Copy, Clone)]
+pub struct Event {
+    pub time: DateTime<Utc>,
+    pub action: EventAction,
+}
+
+impl Event {
+    pub fn from_policeman_resignation_event(policeman: &Policeman) -> Event {
+        Event {
+            time: policeman.resignment_date,
+            action: EventAction::PolicemanResignation,
+        }
+    }
+}
+
+impl Eq for Event {}
+
+impl PartialEq<Self> for Event {
+    fn eq(&self, other: &Self) -> bool {
+        self.time == other.time
+    }
+}
+
+impl PartialOrd<Self> for Event {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.time.cmp(&other.time)
+    }
+}
